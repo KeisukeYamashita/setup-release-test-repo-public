1ef504a8a6e29086c2b9ca369aeea58b1dab012f
KeisukeYamashita <19yamashita15@gmail.com>
2020-12-24 16:48:49 +0900

    Intial commit
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KeisukeYamashita/setup-release-test-repo
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var (
+	version = ""
+)
+
+func main() {
+	fmt.Printf("version: %s\n", version)
+}
